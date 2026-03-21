cfd0102acbe994fd5bcb7369a9c9f300f0314aba
Ali Lefta <alilefta95@gmail.com>
2026-03-22 00:07:51 +0300

    simple test method to check for Lab creation after onboarding

---
+++ data/lab.ts
@@ -0,0 +1,23 @@
+// Used by server components only!
+
+import { ERRORS } from "@/lib/errors";
+import { getServerSession } from "@/lib/get-session";
+import { tenantPrisma } from "@/lib/prisma";
+
+export async function getLabInfo() {
+	const session = await getServerSession();
+	if (!session) throw ERRORS.UNAUTHORIZED;
+
+	if (!session.session.labId) {
+		throw ERRORS.UNAUTHORIZED;
+	}
+	const lab = await (
+		await tenantPrisma(session.session.labId)
+	).lab.findUnique({
+		where: {
+			id: session.session.labId,
+		},
+	});
+
+	return lab;
+}
