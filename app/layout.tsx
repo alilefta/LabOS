549df2cdd721f3f255b7a0bee32a498c144eb6da
Ali Lefta <alilefta95@gmail.com>
2026-03-22 00:12:22 +0300

    added SSR hydration for uploadThing

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,10 @@ import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/providers/theme-provider";
 
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+import { labOSUploadRouter } from "@/app/api/uploadthing/core";
+
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 const jetBrainMono = JetBrains_Mono({
@@ -26,6 +30,7 @@ export default function RootLayout({
 		<html lang="en" className={cn("font-sans", inter.variable)} suppressHydrationWarning>
 			<body className={` ${jetBrainMono.variable} ${inter.variable} antialiased`}>
 				<ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+					<NextSSRPlugin routerConfig={extractRouterConfig(labOSUploadRouter)} />
 					{children}
 					<Toaster />
 				</ThemeProvider>
