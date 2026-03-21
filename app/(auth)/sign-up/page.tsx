cdd149b77f485857b177abb947786e8bb03a9502
Ali Lefta <alilefta95@gmail.com>
2026-03-22 00:09:13 +0300

    removed unwanted redirection

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,12 +1,5 @@
 import { SignupForm } from "@/components/auth/sign-up-form";
-import { getServerSession } from "@/lib/get-session";
-import { redirect } from "next/navigation";
-
 export default async function SignUpPage() {
-	const session = await getServerSession();
-	if (!session?.session) {
-		redirect("/dashboard");
-	}
 	return (
 		<main className="w-full flex items-center justify-center h-screen">
 			<SignupForm />
