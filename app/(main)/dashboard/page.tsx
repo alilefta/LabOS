e56b9471af3022809704f4d11d5c38e8b585dbe6
Ali Lefta <alilefta95@gmail.com>
2026-03-22 00:10:52 +0300

    simple Lab retrieve after successful onboard

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -1,7 +1,23 @@
-export default function DashboardPage() {
+import { getLabInfo } from "@/data/lab";
+import { getServerSession } from "@/lib/get-session";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+export default async function DashboardPage() {
+	const session = await getServerSession();
+
+	if (!session) redirect("/sign-in");
+
+	const lab = await getLabInfo();
+
+	if (!lab) {
+		throw new Error("No Lab Exists!");
+	}
+
 	return (
 		<main className="w-full px-8 py-4">
-			<h1 className="text-4xl">Welcome to Dashboard page</h1>
+			<h1 className="text-4xl">Welcome to Dashboard of {lab.title}</h1>
+			{lab.brandAvatarUrl && <Image src={lab.brandAvatarUrl} width={800} height={800} alt={lab.title} />}
 		</main>
 	);
 }
