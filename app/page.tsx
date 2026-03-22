2bfe81793f3a2e095424aeee9c5426fa3150d3e0
Ali Lefta <alilefta95@gmail.com>
2026-03-23 01:02:16 +0300

    started phase 2 of building the core of labOs

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import { ThemeToggle } from "@/components/ui/custom/theme-toggle";
+import { UserAvatar } from "@/components/user/user-avatar";
+import { getServerSession } from "@/lib/get-session";
 
-export default function Home() {
+export default async function Home() {
+	const session = await getServerSession();
+	const user = session?.user;
+
 	return (
 		<div className="h-dvh  px-8  w-screen">
 			<header className="flex items-center justify-between py-5">
 				<h2 className="text-2xl font-mono">LabOS</h2>
 				<ThemeToggle />
+				{user && <UserAvatar />}
 			</header>
 			<main className="px-5 w-full flex items-center justify-center h-full">
 				<h1 className="text-3xl font-sans">Landing Page</h1>
